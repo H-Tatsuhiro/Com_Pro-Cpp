d8e1b27ff7ced62ff2d8e294cb1d58ae3c04ae06
H.Tatsuhiro <admin@h-tatsu.com>
2022-02-20 23:14:15 +0900

    Refactor code

--- AtCoder/abc240/c/main.cpp
+++ AtCoder/abc240/c/main.cpp
@@ -2,28 +2,19 @@
 #include <cstring>
 #include <algorithm>
 using namespace std;
-
-// 入力
-int n, A;
-int a[110], b[110];
-
-// DPテーブル
 bool dp[110][10010];
-
 int main() {
-    cin >> n >> A;
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n, 0), b(n, 0);
     for (int i = 0; i < n; ++i) cin >> a[i] >> b[i];
-
-    memset(dp, 0, sizeof(dp));   // 一旦すべて false に
-    dp[0][0] = true;             // dp[0][0] だけ true に
-
+    memset(dp, 0, sizeof(dp));
+    dp[0][0] = true;
     for (int i = 0; i < n; ++i) {
-        for (int j = 0; j <= A; ++j) {
-            if (j >= b[i]) dp[i+1][j] |= dp[i][j-b[i]];
-            if (j >= a[i]) dp[i+1][j] |= dp[i][j-a[i]];
+        for (int j = 0; j <= x; ++j) {
+            if (j >= b[i]) dp[i + 1][j] |= dp[i][j - b[i]];
+            if (j >= a[i]) dp[i + 1][j] |= dp[i][j - a[i]];
         }
     }
-
-    if (dp[n][A]) cout << "Yes" << endl;
-    else cout << "No" << endl;
+    cout << (dp[n][x] ? "Yes" : "No") << endl;
 }
