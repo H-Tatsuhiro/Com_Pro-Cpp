7496ca2e0621942b41aa8ba89a842a40ae035026
H-Tatsuhiro <admin@h-tatsu.com>
2024-02-05 00:00:13 +0900

    Update

--- acc_config/cpp/main.cpp
+++ acc_config/cpp/main.cpp
@@ -1,8 +1,19 @@
+/*
+ *
+ * Author: Tatsuhiro Hashimoto
+ *
+ */
 #include <iostream>
+#include <iomanip>
 #include <cmath>
-#include <algorithm>
 #include <vector>
+#include <set>
+#include <queue>
+#include <unordered_map>
+#include <algorithm>
+#include <atcoder/all>
 using namespace std;
 int main() {
 
+    return 0;
 }
